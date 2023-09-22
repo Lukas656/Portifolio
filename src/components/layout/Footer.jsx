fc19d5c83f572d801480e351f6c6e99251fff9e7
Lucas <Lukas656>
2023-09-22 14:21:11 -0300

    Footer Finalizado

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,25 @@ import './layout.css';
 
 const Footer = props => (
     <div className="footer">
-        <header>Footer Pronto !!!</header>
+        <div className="footer-content">
+        <div className="contact-info">
+          <h3>Contato</h3>
+          <p>Email: lucassantosnasc.21@gmail.com</p>
+          <p>Telefone: (11) 95381-9876</p>
+          <p>Estado: São Paulo</p>
+        </div>
+        <div className="social-links">
+          <h3>Redes Sociais</h3>
+          <ul>
+            <li><a href="https://github.com/Lukas656/Portifolio" target="_blank" rel="noopener noreferrer">Repositório Github.com</a></li>
+            <li><a href="https://www.linkedin.com/in/link-lucas-santos/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+          </ul>
+        </div>
+        </div>
+        
+      <div className="copyright">
+        <span>&copy; {new Date().getFullYear()} Feito por: Lucas Santos Nascimento.</span>
+      </div>
     </div>
 )
 
